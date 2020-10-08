fecba2aaaeb43a9a839236d2f67fde758aecd826
galka-travinskaya <galka03031988@rambler.ru>
2020-10-08 16:39:43 +0300

    test

--- js/task-8-gallery.js
+++ js/task-8-gallery.js
@@ -1,10 +1,43 @@
-import products from './gallery-items.js';
+import elements from './gallery-items.js';
 
-const galleryList = document.querySelector('.gallery js-gallery');
+const galleryList = document.querySelector('ul.js-gallery');
+const modalWindow = document.querySelector('.lightbox');
 
-const makeGalleryEl = ({preview, original, description}) => {
-    const galleryEl = document.createElement('a');
-    galleryEl.classList.add('gallery__link');
+const imagesCards = createImageCards(elements);
 
-    return galleryEl;
-};
+galleryList.insertAdjacentHTML('beforeend', imagesCards);
+
+galleryList.addEventListener('click', onGalleryListClick);
+
+console.log(createImageCards(elements));
+
+function createImageCards(elements) {
+    return elements
+    .map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join('');
+}
+
+function onGalleryListClick(evt) {
+  modalWindow.classList.add('is-open');
+  const openImg = evt.target.dataset.source;
+  if(!openImg) {
+    return;
+  }
+
+
+    console.log(evt.target);
+}

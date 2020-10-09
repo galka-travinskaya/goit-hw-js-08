5acf6385345ed72a0433a572d7dbfa3aa749cf66
galka-travinskaya <galka03031988@rambler.ru>
2020-10-09 22:49:06 +0300

    добавляет галлерею

--- js/task-8-gallery.js
+++ js/task-8-gallery.js
@@ -11,15 +11,39 @@ const refs = {
 const imagesCards = createImageCards(elements);
 
 refs.galleryContainer.insertAdjacentHTML('beforeend', imagesCards);
-refs.galleryContainer.addEventListener('click', onGalleryListClick);
+// refs.galleryContainer.insertAdjacentHTML('beforeend', createImageCards);
+
+refs.galleryContainer.addEventListener('click', onImageClick);
+refs.galleryContainer.addEventListener('click', onOpenModal);
+refs.modalOverlay.addEventListener('click', onCloseModal);
+refs.modalBtn.addEventListener('click', onCloseModal);
+
 
 console.log(createImageCards(elements));
+
+// function createImageCards({preview, original, description}) {
+//   const galleryItem = document.createElement('li');
+//         galleryItem.classList.add('gallery__item');
+        
+//       const galleryLink = document.createElement('a');
+//       galleryLink.classList.add('gallery__link');
+//       galleryLink.href = original;
+
+//       const galleryImg = document.createElement(img);
+//       galleryImg.classList.add('gallery__image');
+//       galleryImg.src = preview;
+//       galleryImg.dataset.source = original;
+//       galleryImg.alt = description;
+
+//       galleryItem.append(galleryLink, galleryImg);
+//       return galleryItem;
+// }
 
 function createImageCards(elements) {
     return elements
     .map(({preview, original, description}) => {
-        return `
-        <li class="gallery__item">
+      return `
+      <li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -35,11 +59,35 @@ function createImageCards(elements) {
     }).join('');
 }
 
-function onGalleryListClick(evt) {
-  refs.modal.classList.add('is-open');
-  const openImg = evt.target.dataset.source;
-  if(!openImg) {
-    return;
-  }
-    console.log(evt.target);
+
+function onImageClick(event) {
+  event.preventDefault();
+  // return event.target.dataset.sourse;
 }
+
+
+function onOpenModal(evt) {
+  const activeImg = document.querySelector('is-open');
+  refs.modalImg.src = evt.target.dataset.source;
+  if(activeImg) {
+    refs.modal.classList.remove('is-open');
+  }
+  refs.modal.classList.add('is-open');
+  window.addEventListener('keydown', onEscKeyPress);
+}
+
+function onCloseModal(evt) {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.modal.classList.remove("is-open");
+  refs.modalImg.src = "";
+}
+
+function onEscKeyPress(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+
+  if(isEscKey) {
+    onCloseModal(evt);
+  }
+}
+
